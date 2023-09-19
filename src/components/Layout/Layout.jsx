c60c09b10ec0aec11038ab347a2a9212686e2a93
groguls <groguls@gmail.com>
2023-09-20 00:13:47 +0200

    fixed header styles

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,19 +1,27 @@
 import { Suspense } from 'react';
 import { Link, NavLink, Outlet } from 'react-router-dom';
-import { Footer, Header, Navigation, StyledLayout } from './Layout.styled';
+import {
+  Footer,
+  Header,
+  HeaderContainer,
+  Navigation,
+  StyledLayout,
+} from './Layout.styled';
 import { ReactComponent as Logo } from '../../images/logo.svg';
 
 export default function Layout() {
   return (
     <StyledLayout>
       <Header>
-        <Link to="/">
-          <Logo />
-        </Link>
-        <Navigation>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/movies">Movies</NavLink>
-        </Navigation>
+        <HeaderContainer>
+          <Link to="/">
+            <Logo />
+          </Link>
+          <Navigation>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/movies">Movies</NavLink>
+          </Navigation>
+        </HeaderContainer>
       </Header>
       <main>
         <Suspense>
--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -13,15 +13,21 @@ export const Header = styled.header`
   top: 0;
   left: 0;
   width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   background-color: rgb(13, 37, 63);
   height: 64px;
   color: #fff;
   font-weight: 600;
   z-index: 10;
-  padding: 0 120px;
+`;
+
+export const HeaderContainer = styled.div`
+  max-width: 1440px;
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+  margin: 0 auto;
+  padding: 0 20px;
+  align-items: center;
 `;
 
 export const Container = styled.div`
@@ -42,8 +48,6 @@ export const Navigation = styled.nav`
   justify-content: flex-end;
   align-items: center;
   gap: 40px;
-  width: 100%;
-  padding: 0 100px;
 `;
 
 export const Footer = styled.footer`
